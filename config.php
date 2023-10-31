71a54c1b43f6f74ad5a9fdb8e3754d94c65a6c5e
asadmu92 <asadmughal88@gmail.com>
2023-10-31 18:10:39 +0500

    update config file

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php 
 
 // google drive  
-$driveClientId = '730173637048-32f7k82j3dfoqcknnsisk38l9cpet1jg.apps.googleusercontent.com';
-$driveClientSecret = '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';
+$driveClientId = 'YOUR_DRIVE_CLIENT_ID';
+$driveClientSecret = 'YOUR_DRIVE_CLIENT_SECRET';
 
 
 // dropbox 
-$dropboxRedirectUri = 'http://localhost:90/test_dropbox/connect_dropbox.php';
-$dropboxClientId = 'zlqtkkl89xc5jkb'; // Replace with your Dropbox app's client ID
-$dropboxClientSecret = 'xk2jn0hb42n1r95'; // Replace with your Dropbox app's client secret
+$dropboxRedirectUri = 'YOUR_DROPBOX_REDIRECT_URI';
+$dropboxClientId = 'YOUR_DROPBOX_CLIENT_ID'; // Replace with your Dropbox app's client ID
+$dropboxClientSecret = 'YOUR_DROPBOX_CLIENT_SECRET'; // Replace with your Dropbox app's client secret
 
 
 
